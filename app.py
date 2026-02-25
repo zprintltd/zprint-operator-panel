83fd4b8c1f6dbfec542d77b118a45490923f4955
zprintltd <idara.zprint@gmail.com>
2026-02-25 22:48:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+# ----------------------------
+# CUSTOM CSS (BUTTON STYLING)
+# ----------------------------
 st.markdown("""
 <style>
 div.stButton > button {
